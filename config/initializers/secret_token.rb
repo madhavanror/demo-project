02f095db8baf5629421d88e9af1df72374b2fa24
MadhavanRajarethinam <madhavan.rajarethinam@nuware.com>
2017-01-30 16:04:47 +0530

    Create Empty Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c71b1d40bfaeeb545582993a7de8bd42dddb1c5e50ab2f476c6c7ad7b74f398fd9f14915df150151d0865d31d3041f9954177bf70a727423a27da129a700158f'
